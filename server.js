c002ba298de9bc5766d4c24692945503a7a1d6f9
Mohamed Fayis <fayis@outlook.in>
2018-08-21 12:44:19 +0530

    hello changed

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 let port = process.env.port || 3000
 
-app.get('/', (req, res) => res.send('hello'))
+app.get('/', (req, res) => res.send('helloh'))
 
 app.listen(port)
